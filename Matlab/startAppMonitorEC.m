6bdb9c940ac9ef3a301d918270526f7d382a4ac9
flux-central <flux-central@outlook.com>
2024-08-11 20:07:36 -0500

    Made sure that the input path in startAppMonitorEC is a char not a string

--- Matlab/startAppMonitorEC.m
+++ Matlab/startAppMonitorEC.m
@@ -1,6 +1,6 @@
 xx=set_TAB_project('z:\Projects\UQAM');
 addpath('C:\Biomet.net\matlab\Apps\')
-appMonitorLicorEC("z:\Projects\UQAM")
+appMonitorLicorEC('z:\Projects\UQAM')
 clc
 fprintf(2,' **** DO NOT USE THIS WINDOW ****\n');
 
